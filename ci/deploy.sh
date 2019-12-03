7607c5a078828ad21120c0a6d615edd79755a1d3
Emmanuel Fallancy <emmanuel.fallancy@gmail.com>
2019-12-04 05:02:59 +1100

    Update deployment script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -6,4 +6,4 @@ app=$1
 
 # This script deploys to Heroku.
 # Make sure the app has already been created.
-git push https://heroku:$HEROKU_API_KEY@git.heroku.com/$app.git master
+git push https://heroku:$HEROKU_API_KEY@git.heroku.com/$app.git origin/master:master
